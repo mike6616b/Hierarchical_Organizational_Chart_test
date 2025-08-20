aeb72583316985d261a1b8271a8082c812233f63
mike6616b <130122494+mike6616b@users.noreply.github.com>
2025-08-20 17:20:26 +0800

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+  base: '/Hierarchical_Organizational_Chart_test/'
+})
